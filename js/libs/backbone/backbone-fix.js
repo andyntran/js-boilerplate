e879ab6adf6c667d49dc36ca5861a4b27e7262d8
Andy Tran <owner@ND-LAPTOP.(none)>
2012-09-14 02:42:01 -0700

    Moved backbone fix to js/backbone/backboneFix.js. Updated various js libs. Set CDN urls for common libs with local fallback urls.

---
+++ js/libs/backbone/backbone-fix.js
@@ -0,0 +1,7 @@
+// Fixed problem with Backbone put query string in URL routing parameter
+(function(old) {
+    Backbone.History.prototype.getFragment = function() {
+        return old.apply(this, arguments).replace(/\?.*/, '');
+    };
+})(Backbone.History.prototype.getFragment);
+
