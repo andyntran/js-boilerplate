691bcc3916d066c50678ce5e4460e8b54229d5d6
Andy Tran <owner@ND-LAPTOP.(none)>
2012-09-14 03:43:54 -0700

    Added main.build.js to support using r.js to combine all js files into 1 single js file.

---
+++ js/main.build.js
@@ -0,0 +1,48 @@
+// NOTE: Use the following command to run this build file in the application folder (js-boilerplate)
+// node js/libs/requirejs/r-2.0.6.js -o js/main.build.js
+
+({
+      appDir: "../"
+    , baseUrl: "./js/"
+    
+    // TODO: Changes "dir" value to preferred folder
+    , dir: "../../js-boilerplate-build"
+    // END TODO
+
+    , paths: {
+          text: 'libs/requirejs/require-text.min'
+        , css: 'libs/requirejs/require-css.min'
+        , modernizr: 'libs/modernizr/modernizr-2.6.2.min'
+        , jquery: 'libs/jquery/jquery-1.8.1.min'
+        , underscore: 'libs/underscore/underscore-1.3.3.min'
+        , backbone: 'libs/backbone/backbone-0.9.2.min'
+        , backboneFix: 'libs/backbone/backbone-fix'
+
+        // TODO: Uncomments the following to set alias for additional libraries
+        // , jqueryui: 'libs/jquery/jqueryui/jquery-ui-1.8.23.min'
+        // , bootstrap: 'libs/bootstrap/bootstrap-2.1.1.min'
+        // , handlebars: 'libs/handlebars/handlebars-1.0.0.beta.6.min'
+        // END TODO
+    }
+    , shim: {
+          'backbone': ['jquery', 'underscore']
+        , 'backboneFix': ['backbone']
+        , 'router': [
+              'modernizr'
+            , 'jquery'
+            , 'backbone'
+            , 'backboneFix'
+
+            // TODO: Uncomments the following to includes loading additional libraries
+            // , 'jqueryui'
+            // , 'bootstrap'
+            // , 'handlebars'
+            // END TODO
+        ]
+
+        // TODO: Uncomments the following to set jQuery dependency for jQuery UI
+        // , 'jqueryui': ['jquery']
+        // END TODO
+    }
+    , modules: [{ name: "main" }]
+})
--- js/main.js
+++ js/main.js
@@ -11,29 +11,49 @@ requirejs.config({
 		, css: 'libs/requirejs/require-css.min'
 
 		// Core libraries
-		, modernizr: 'libs/modernizr/modernizr-2.6.1.min'
-		, jquery: 'libs/jquery/jquery-1.8.0.min'
-		, underscore: 'libs/underscore/underscore-1.3.3.min'
-		, backbone: 'libs/backbone/backbone-0.9.2.min'
+		, modernizr: [
+			  '//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min'
+			, 'libs/modernizr/modernizr-2.6.2.min'
+		]
+		, jquery: [
+			  '//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min'
+			, 'libs/jquery/jquery-1.8.1.min'
+		]
+		, underscore: [
+			  '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min'
+			, 'libs/underscore/underscore-1.3.3.min'
+		]
+		, backbone: [
+			  '//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone-min'
+			, 'libs/backbone/backbone-0.9.2.min'
+		]
+		, backboneFix: 'libs/backbone/backbone-fix'
 
 		// TODO: Uncomments the following to set alias for additional libraries
-		// , jqueryui: 'libs/jquery/jqueryui/jquery-ui-1.8.23.min'
-		// , bootstrap: 'libs/bootstrap/bootstrap-2.1.0.min'
-		// , handlebars: 'libs/handlebars/handlebars-1.0.0.beta.6.min'
+		// , jqueryui: [
+		// 	  '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.23/jquery-ui.min'
+		// 	, 'libs/jquery/jqueryui/jquery-ui-1.8.23.min'
+		// ]
+		// , bootstrap: [
+		// 	  '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.1.1/bootstrap.min'
+		// 	, 'libs/bootstrap/bootstrap-2.1.1.min'
+		// ]
+		// , handlebars: [
+		// 	  '//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0.beta6/handlebars.min'
+		// 	, 'libs/handlebars/handlebars-1.0.0.beta.6.min'
+		// ]
 		// END TODO
-
-		// Main components
-		, router: 'router'
-		, specsRunner: 'specs/specs-runner'
 	}
 
 	// Defines dependencies (effectively sets the loading orders)
 	, shim: {
 		  'backbone': ['jquery', 'underscore']
+		, 'backboneFix': ['backbone']
 		, 'router': [
 			  'modernizr'
 			, 'jquery'
 			, 'backbone'
+			, 'backboneFix'
 
 			// TODO: Uncomments the following to includes loading additional libraries
 			// , 'jqueryui'
